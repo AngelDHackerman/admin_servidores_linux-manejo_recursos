3536f94af34febfb394f9ffa768a34616461b728
angelDariaux <angeldariaux@gmail.com>
2023-03-15 21:51:40 -0600

    mostrar la version de grup en ubuntu y fedora

---
+++ version_grub.bash
@@ -0,0 +1,11 @@
+
+# Con este comando puedes ver la version de grub que tienes instalado: 
+
+# * para ver la version en fedora: 
+
+sudo grub2-install --version
+
+
+# * para ver la version en ubuntu: 
+
+sudo grub2-install --version

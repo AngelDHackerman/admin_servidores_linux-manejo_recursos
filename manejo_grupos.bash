bd7e0f85e384e913d619ff5c8e1c7c62238746c7
angelDariaux <angeldariaux@gmail.com>
2023-03-18 22:29:55 -0600

    ver los grupos a los que se pertenece, agregar a un nuevo grupo

---
+++ manejo_grupos.bash
@@ -0,0 +1,32 @@
+
+# ? Ver a los grupos que pertenece el usuario actual: 
+
+groups # muestra los grupos a los que perteneces. 
+
+# ? Ver que usuarios perteneces a ciertos grupos: 
+
+getent group nombre_del_grupo
+
+
+# ? Crear un grupo nuevo: 
+
+sudo groupadd nombre_del_nuevo_grupo
+
+# ? Ver todos los grupos existentes en linux: 
+
+cat /etc/group
+
+
+# ? Cambiar el nombre del grupo: 
+
+sudo groupmod -n nuevo_nombre antiguo_nombre
+
+
+# ? Cambiar el grupo al que pertenece un archivo o carpeta (e.g. para poder compartir un archivo entre varios grupos)
+
+sudo chgrp grupo_a_compartir nombre_carpeta
+
+
+# ? Agregar a un usuario a otro grupo: 
+
+sudo usermod -aG nombre_del_grupo nombre_del_usuario

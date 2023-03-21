e4c5ca9007a961150b7d0e8a411e3d1c95c53e21
angelDariaux <angeldariaux@gmail.com>
2023-03-20 22:34:28 -0600

    creando copia de seguridad del grub

---
+++ recuperando_grub.bash/recuperar_grup.bash
@@ -0,0 +1,12 @@
+
+# Ver el archivo de configuracion grub 
+
+sudo cat /boot/grub2/grub.cfg
+
+# crear copia de seguridad de grub: 
+
+sudo cp /boot/grub2/grub.cfg /boot/grub2/grub.cfg.bak
+
+
+# ? Si el archivo de "grub.cfg" es dañado, al hacer reboot, nos dara un promt en negro
+

6c17a8a5cbafa4aa5d7a055ee46a367ecc971eb0
angelDariaux <angeldariaux@gmail.com>
2023-03-20 20:47:14 -0600

    termianda la creacion del raid 1

--- crear_sistema_RAID1.bash
+++ crear_sistema_RAID1.bash
@@ -27,3 +27,28 @@ Last sector # enter (default)
 Command (m for help): w  # Se guardaran los cambios
 
 
+# ? Creando el sistema RAID 1: 
+
+sudo apt install mdadm  # ubuntu 
+
+sudo dnf install mdadm  # fedora 
+
+
+mdadm -h  # muestra la ayuda del comando mdadm (varias de las opciones)
+
+mdadm --create --help # Muestra todas las opciones disponibles con el parametro --create
+
+# crear el arreglo raid: 
+
+        # nombre arreglo          N. discos (2)     Ruta de discos [cd] es para el disco "c" y el disco "d"
+mdadm --create /dev/md0 --level=1 --raid-devices=2 /dev/sd[cd]
+
+Continue creating array? yes # (enter). Aceptamos los cambios 
+
+
+# ? Ver los detalles del nuevo array de discos: 
+
+lsblk  # listamos y verificamos la creacion del nuevo RAID 1 
+
+mdadm --misc --detail /dev/md0  # muestra toda la informacion sobre el array de discos
+
